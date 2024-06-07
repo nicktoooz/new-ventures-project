249b0fb239bc496e12104f60ea5e8e61b365be9f
Dominic Mendoza Enriquez <dome.enriquez.up@phinmaed.com>
2024-06-07 22:12:35 +0800

    Updated mailer

--- src/app/api/send-mail/route.ts
+++ src/app/api/send-mail/route.ts
@@ -4,30 +4,47 @@ import { createTransport } from 'nodemailer';
 async function handler(req: NextRequest) {
   try {
     const { name, streetAddress, suburb, email, mobileNumber, service, details } = await req.json();
+
     const html = `
-          <div>
-            <div style="background-color: #788bff; display: flex; padding: 1.5em; justify-content: space-between; align-items: center">
-              <h1 style="margin: 0; color: #fff; font-family: Arial, Helvetica, sans-serif">New Request</h1>
-            </div>
-            <div style="background-color: #99afff20; padding: 1em">
-              <div style="display: grid; grid-template-columns: 10em auto">
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Name:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${name}</p>
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Address:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${streetAddress}</p>
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Suburb:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${suburb}</p>
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Email:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${email}</p>
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Mobile Number:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${mobileNumber}</p>
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Service:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${service}</p>
-                <p style="font-family: Arial, Helvetica, sans-serif; font-weight: bold">Details:</p>
-                <p style="font-family: Arial, Helvetica, sans-serif">${details}</p>
-                <a style="background-color: #5465ff; width: fit-content; padding: 12px 50px; font-family: Arial, Helvetica, sans-serif; text-decoration: none; color: white; border-radius: 4px" href="mailto:${email}">Reply</a>
-              </div>
-            </div>
+          <div style="background-color: #99afff20">
+            <table style="background-color: #99afff20; padding: 20px; width: 100%;">
+                <tr>
+                    <td style="background-color: #788bff; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 15px; text-align: start;">
+                        <h1 style="margin: 0;">New Request</h1>
+                    </td>
+                </tr>
+            </table>
+            <table style="background-color: #99afff20; padding: 20px; width: 100%; margin-bottom: 50px;">
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; width: 10em; padding-top: 12px;">Name:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${name}</td>
+                </tr>
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; padding-top: 12px;">Address:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${streetAddress}</td>
+                </tr>
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; padding-top: 12px;">Suburb:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${suburb}</td>
+                </tr>
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; padding-top: 12px;">Email:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${email}</td>
+                </tr>
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; padding-top: 12px;">Mobile Number:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${mobileNumber}</td>
+                </tr>
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; padding-top: 12px;">Service:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${service}</td>
+                </tr>
+                <tr>
+                    <td style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; padding-top: 12px;">Details:</td>
+                    <td style="font-family: Arial, Helvetica, sans-serif; padding-top: 12px;">${details}</td>
+                </tr>
+            </table>
+            <a style="background-color: #5465ff; width: fit-content; padding: 12px 50px; font-family: Arial, Helvetica, sans-serif; text-decoration: none; color: white; border-radius: 4px" href="mailto:${email}">Reply</a>
           </div>
         `;
 
@@ -39,6 +56,49 @@ async function handler(req: NextRequest) {
         user: 'service@dondenciong.com.au',
         pass: 'tB37pvpH.29A',
       },
+    });
+
+    const endpoint = createTransport({
+      host: 'mail.dondenciong.com.au',
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'info@dondenciong.com.au',
+        pass: 'eVKy59vLghe2xMr',
+      },
+    });
+
+    await endpoint.sendMail({
+      from: 'info@dondenciong.com.au',
+      to: email,
+      subject: 'Cleaning Enquiry Received',
+      html: `
+      <table style="font-family: Arial, Helvetica, sans-serif">
+        <tr>
+          <td>
+            <table style="background-color: #99afff20; width: 100%">
+              <tr>
+                <td style="background-color: #788bff; color: #fff; padding: 30px 15px; text-align: center">
+                  <h1 style="margin: 0; font-size: 40px">Enquiry Received</h1>
+                </td>
+              </tr>
+            </table>
+            <table style="background-color: #99afff20; padding: 50px 25px; font-size: 25px">
+              <tr>
+                <td><p>Thank you for your enquiry regarding our cleaning services. We appreciate the opportunity to assist you with your cleaning needs. We've received your request and will review it carefully to ensure we understand your requirements fully. Our team will reach out shortly to discuss your specific needs and provide you with a tailored cleaning solution. Thank you for considering us, and we look forward to working with you!</p></td>
+              </tr>
+              <tr>
+                <td><p style="padding-top: 60px">Best Regards</p></td>
+              </tr>
+              <tr>
+                <td><p>Don Denciong Cleaning Services</p></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+
+      `,
     });
 
     await transporter.sendMail({
