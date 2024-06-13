38f0a2950e18ba2d0fa6c2350da84ea181eb32a1
lancife05 <gamboafreelan@gmail.com>
2024-06-14 00:39:23 +0800

    Added Menu Animation

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,37 +1,48 @@
 'use client'
-import Image from "next/image";
-import menu from "../../public/icons8-menu.svg";
 import {useState} from "react";
 
 export default function Header() {
     const [menuVisibility, setMenu] = useState(false)
 
-    return <header className={`h-16 shadow-xl flex justify-between px-5 items-center `}>
-        <Menu visible={menuVisibility}/>
-        <div>
-            <h1>Don Denciong</h1>
-        </div>
-        <div className={`header-right `}>
-            <Image onClick={() => setMenu(!menuVisibility)} className={`md:hidden`} src={menu} alt={``}/>
-            <div className={` hidden gap-10 md:flex `}>
-                <button className={`hover:underline decoration-4 decoration-[#5465FF] `}>Home</button>
-                <button className={`hover:underline decoration-4 decoration-[#5465FF] `}>Services</button>
-                <button className={`hover:underline decoration-4 decoration-[#5465FF] `}>About Us</button>
-                <button
-                    className={`border-2 border-[#5465FF] hover:bg-[#5465FF] hover:text-white p-2 rounded-md text-[#5465FF]`}>Contact
-                    Us
-                </button>
-            </div>
-        </div>
-    </header>
-}
-function Menu({visible}:{visible:boolean}) {
     return (
-        visible && (
-            <div className={`w-full mt-16 h-96 bg-slate-500 absolute top-0 left-0`}>
+        <>
+            <header className={`h-16 relative bg-white z-40 shadow-xl flex justify-between px-5 items-center `}>
+                <div>
+                    <h1>Don Denciong</h1>
+                </div>
+                <div className={`header-right z-50`}>
+                    <div className={`md:hidden`} onClick={() => setMenu(!menuVisibility)}>
+                <span className={`bg-black h-1.5 w-10 rounded block transition-all duration-500`} style={{
+                    rotate: `${menuVisibility ? '45deg' : '0deg'}`,
+                    transform: `${menuVisibility ? 'translateY(10px) translateX(4px)' : 'translateY(0px)'}`
+                }}></span>
+                        <span className={`bg-black h-1.5 w-10 rounded block transition-all duration-500 my-1`}
+                              style={{opacity: `${menuVisibility ? 0 : 1}`}}></span>
+                        <span className={`bg-black h-1.5 w-10 rounded block transition-all duration-500`} style={{
+                            rotate: `${menuVisibility ? '-45deg' : '0deg'}`,
+                            transform: `${menuVisibility ? 'translateY(-10px) translateX(4px)' : 'translateY(0px)'}`
+                        }}></span>
 
+                    </div>
+                    <div className={` hidden gap-10 md:flex `}>
+                        <button className={`hover:underline decoration-4 decoration-[#5465FF] `}>Home</button>
+                        <button className={`hover:underline decoration-4 decoration-[#5465FF] `}>Services</button>
+                        <button className={`hover:underline decoration-4 decoration-[#5465FF] `}>About Us</button>
+                        <button
+                            className={`border-2 border-[#5465FF] hover:bg-[#5465FF] hover:text-white p-2 rounded-md text-[#5465FF]`}>Contact
+                            Us
+                        </button>
+                    </div>
+                </div>
+            </header>
+            <Menu visible={menuVisibility}/>
+        </>)
+}
 
-            </div>
-        )
+function Menu({visible}: { visible: boolean }) {
+    return (
+        <div className={`z-20 w-full mt-16 h-96 bg-slate-500 absolute top-0 left-0 transition-all duration-500`}
+             style={{transform: `translateY(${visible ? '0' : '-100%'})`}}>
+        </div>
     )
 }
