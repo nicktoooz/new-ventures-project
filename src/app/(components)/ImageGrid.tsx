9ba73b14f73b6e3aa3b483a4f33b64b17180be91
lancifer05 <gamboafreelan@gmail.com>
2024-06-19 02:00:22 +0800

    updated image resolution

--- src/app/(components)/ImageGrid.tsx
+++ src/app/(components)/ImageGrid.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function ImageGrid() {
 
-    const images = ['https://picsum.photos/200?1', 'https://picsum.photos/200?2', 'https://picsum.photos/200?3', 'https://picsum.photos/200?4']
+    const images = ['https://picsum.photos/1000?1', 'https://picsum.photos/1000?2', 'https://picsum.photos/1000?3', 'https://picsum.photos/1000?4']
     const [currentImage, setCurrentImage] = useState(images[0])
 
     const handleClick = (index: number) => {
@@ -14,11 +14,11 @@ export default function ImageGrid() {
     return (
         <div className="preview justify-self-center grid aspect-square w-80 grid-cols-4 gap-2.5">
             <div className="main bg-red-600 aspect-square col-span-4">
-                <Image src={currentImage} alt={'Current Image'} className={`w-full`} width={200} height={200} objectFit="cover" />
+                <Image src={currentImage} alt={'Current Image'} className={`w-full`} width={700} height={700} objectFit="cover" />
             </div>
             {images.map((image, index) => (
                 <div key={index} className="aspect-square bg-blue-500" onClick={() => handleClick(index)}>
-                    <Image src={image} className={'w-full'} alt={`Image ${index + 1}`} width={200} height={200} objectFit="cover" />
+                    <Image src={image} className={'w-full'} alt={`Image ${index + 1}`} width={700} height={700} objectFit="cover" />
                 </div>
             ))}
         </div>
