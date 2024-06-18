41c040b15d2e93877a60767c28da9fcac8fc28d2
lancifer05 <gamboafreelan@gmail.com>
2024-06-19 02:05:57 +0800

    Added Transition on hover

--- src/app/(components)/ImageGrid.tsx
+++ src/app/(components)/ImageGrid.tsx
@@ -13,12 +13,12 @@ export default function ImageGrid() {
 
     return (
         <div className="preview justify-self-center grid aspect-square w-80 grid-cols-4 gap-2.5">
-            <div className="main bg-red-600 aspect-square col-span-4">
-                <Image src={currentImage} alt={'Current Image'} className={`w-full`} width={700} height={700} objectFit="cover" />
+            <div className="main bg-red-600 h-80 overflow-hidden col-span-4">
+                <Image src={currentImage} alt={'Current Image'} className={`w-full hover:scale-125 transition-all duration-200`} width={700} height={700} objectFit="cover" />
             </div>
             {images.map((image, index) => (
-                <div key={index} className="aspect-square bg-blue-500" onClick={() => handleClick(index)}>
-                    <Image src={image} className={'w-full'} alt={`Image ${index + 1}`} width={700} height={700} objectFit="cover" />
+                <div key={index} className="aspect-square bg-blue-500 " onClick={() => handleClick(index)}>
+                    <Image src={image} className={'w-full '} alt={`Image ${index + 1}`} width={700} height={700} objectFit="cover" />
                 </div>
             ))}
         </div>
